565b6d9b15b31ed5d37e3bf507b7c22144f51078
Reuben Hawkins <hreuben@amazon.com>
2019-01-29 06:20:14 +0000

    add setuptools install script

---
+++ InternetCanary/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import sys
+import time
+
+def _http_probe(url, timeout):
+    try:
+        r = requests.get(url, timeout=timeout)
+        if r.status_code == requests.codes.ok:
+        	return r.elapsed.total_seconds()
+    except:
+        pass
+    return -1
+
+def run():
+	with open(sys.argv[1]) as configfile:
+	    config = json.load(configfile)
+
+	    with open(config['canaries']['http']['logfile'], 'a') as outfile:
+		for target in config['canaries']['http']['targets']:
+		    url = "http://{}".format(target)
+		    now = time.time()
+		    result = _http_probe(url, 5)
+		    outfile.write("http, {}, {}, {}\n".format(now, url, result))
+		
+	    with open(config['canaries']['https']['logfile'], 'a') as outfile:
+		for target in config['canaries']['https']['targets']:
+		    url = "https://{}".format(target)
+		    now = time.time()
+		    result = _http_probe(url, 5)
+		    outfile.write("https, {}, {}, {}\n".format(now, url, result))
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='InternetCanary',
+    version='1.0',
+    packages=['InternetCanary'],
+    zip_safe=False,
+    data_files=[
+	('/etc/init.d', ['internet-canary.conf']),
+	('/etc/internet-canary.d', ['internet-canary.json']),
+    ]
+)
+
