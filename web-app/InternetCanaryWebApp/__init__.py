70ba32fa49be6a57b40c439dafc0d52ac0005f5b
Reuben Hawkins <hreuben@amazon.com>
2019-02-01 05:28:12 +0000

    extract some test from the results db

--- web-app/InternetCanaryWebApp/__init__.py
+++ web-app/InternetCanaryWebApp/__init__.py
@@ -2,9 +2,25 @@
 
 from datetime import datetime
 from flask import Flask, render_template
+import sqlite3
+
+import PyGnuplot as gplot
 
 app = Flask(__name__)
 
 @app.route("/")
 def simple_hello():
-    return render_template('simple-hello.txt', date=str(datetime.now()))
+
+    conn = sqlite3.connect('/var/lib/internet-canary/internet-canary.db')
+
+    c = conn.cursor()
+
+    rows = c.execute('''
+        select time, result from results order by time desc limit 30;
+    ''').fetchall()
+
+    targets = c.execute('''
+        select distinct target from results;
+    ''').fetchall()
+
+    return render_template('simple-hello.txt', targets=targets, rows=rows, date=str(datetime.now()))
