9146094f2f171f7884bfad73d9b499eb6bb3c4fc
Reuben Hawkins <hreuben@amazon.com>
2019-02-02 16:50:53 +0000

    select a time range

--- web-app/InternetCanaryWebApp/__init__.py
+++ web-app/InternetCanaryWebApp/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template
 import sqlite3
-
+import time
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
@@ -21,15 +21,22 @@ def simple_hello():
         select distinct target from results order by target;
     ''').fetchall()
 
+    end = time.time()
+    start = end - 60 * 60 * 8
+
     svgs=list()
     for target in targets:
-	datapoints = 288
         # TODO: This selects the most recent points, but in the wrong order.
-        rows = c.execute("select time, result from results where target = ? order by time desc limit ?;", (target[0], datapoints)).fetchall()
+        rows = c.execute('''
+           select time, result from results
+               where target = ? and time >= ? and time <= ?
+               order by time;
+        ''', (target[0], start, end)).fetchall()
+
         plt.plot([rows[x][1] for x in xrange(len(rows))])
         plt.ylabel('response time')
         plt.ylim(0,1)
-        plt.xlim(0,datapoints)
+        plt.xlim(0,len(rows))
         plt.title(target[0])
         svg=StringIO.StringIO()
         plt.savefig(svg, format='svg')
