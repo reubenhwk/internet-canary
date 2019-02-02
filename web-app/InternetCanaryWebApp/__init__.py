9d35dda1fc2801bfe00bffbe2d5e076005dcfb0b
Reuben Hawkins <hreuben@amazon.com>
2019-02-02 07:05:02 +0000

    render graphs in html as inline svg

--- web-app/InternetCanaryWebApp/__init__.py
+++ web-app/InternetCanaryWebApp/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from datetime import datetime
 from flask import Flask, render_template
 import sqlite3
 
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import StringIO
 
 app = Flask(__name__)
 
@@ -18,21 +18,27 @@ def simple_hello():
     c = conn.cursor()
 
     targets = c.execute('''
-        select distinct target from results;
+        select distinct target from results order by target;
     ''').fetchall()
 
-    i = 0
+    svgs=list()
     for target in targets:
-        rows = c.execute("select time, result from results where target = ? order by time desc limit 120;", target).fetchall()
+	datapoints = 288
+        # TODO: This selects the most recent points, but in the wrong order.
+        rows = c.execute("select time, result from results where target = ? order by time desc limit ?;", (target[0], datapoints)).fetchall()
         plt.plot([rows[x][1] for x in xrange(len(rows))])
         plt.ylabel('response time')
-        plt.title(target)
-        plt.savefig('/tmp/{}.svg'.format(i))
+        plt.ylim(0,1)
+        plt.xlim(0,datapoints)
+        plt.title(target[0])
+        svg=StringIO.StringIO()
+        plt.savefig(svg, format='svg')
         plt.close()
-        i += 1
+        svg.seek(0)
+        svgs.append(svg.buf)
 
     rows = c.execute('''
         select time, result from results order by time desc limit 30;
     ''').fetchall()
 
-    return render_template('simple-hello.txt', targets=targets, rows=rows, date=str(datetime.now()))
+    return render_template('simple-hello.txt', svgs=svgs)
