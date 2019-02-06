51283fb35bf1c6497a149e2515df0e8a2a8b8a47
Reuben Hawkins <hreuben@amazon.com>
2019-02-05 17:01:58 -0800

    use speedtest-cli to measure bandwidth

--- canary/InternetCanary/__init__.py
+++ canary/InternetCanary/__init__.py
@@ -5,6 +5,15 @@ import json
 import sys
 import time
 import sqlite3
+import speedtest
+
+def _speedtest_probe():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    down_speed = st.download(callback=speedtest.do_nothing)
+    up_speed = st.upload(callback=speedtest.do_nothing)
+    return down_speed, up_speed
+
 
 def _http_probe(url, timeout):
     try:
@@ -33,11 +42,24 @@ def run(config_file_path):
         c.execute('''
             CREATE INDEX IF NOT EXISTS target_time ON results (target, time);
         ''')
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS bandwidth_results (
+                time real,
+                down_speed integer,
+                up_speed integer);
+        ''')
+        c.execute('''
+            CREATE INDEX IF NOT EXISTS bandwidth_time ON bandwidth_results (time);
+        ''')
 
         for url in config['canaries']['http']['targets']:
             now = time.time()
             result = _http_probe(url, 5)
             c.execute("INSERT INTO results (type, target, time, result) VALUES ('http', ?, ?, ?)", [url, now, result])
+
+	bandwidth = _speedtest_probe()
+        c.execute("INSERT INTO bandwidth_results (time, down_speed, up_speed) VALUES (?, ?, ?)", [now, bandwidth[0], bandwidth[1]])
+
         conn.commit()
 
         conn.close()
