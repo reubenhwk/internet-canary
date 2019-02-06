23a457c30cefd1346adb136a336d16acdfa5f53d
Reuben Hawkins <hreuben@amazon.com>
2019-02-05 20:48:08 -0800

    add new bandwidth graph

--- web-app/InternetCanaryWebApp/__init__.py
+++ web-app/InternetCanaryWebApp/__init__.py
@@ -18,13 +18,24 @@ def epoch_to_human(t):
 def interpolate(xmin, xmax, ratio):
     return (xmax - xmin) * ratio + xmin
 
-@app.route("/", methods=['GET', 'POST'])
-def simple_hello():
+@app.route("/")
+def index():
+    return '''
+    <html>
+       <ul>
+         <li><a href='http://35.160.69.193/rt'>rt</a></li>
+         <li><a href='http://35.160.69.193/bw'>bw</a></li>
+       </ul>
+    </html>
+    '''
 
-    if request.method == 'POST':
+@app.route("/rt")
+def reponse_time_page():
+
+    try:
         start = int(request.form['start'])
         end = int(request.form['end'])
-    else:
+    except:
         now = time.time()
         start = now - 60 * 60 * 24
         end = now
@@ -67,4 +78,51 @@ def simple_hello():
         svg.seek(0)
         svgs.append(svg.buf)
 
-    return render_template('simple-hello.txt', svgs=svgs)
+    return render_template('rt.txt', svgs=svgs)
+
+@app.route("/bw")
+def bandwidth_page():
+
+    try:
+        start = int(request.form['start'])
+        end = int(request.form['end'])
+    except:
+        now = time.time()
+        start = now - 60 * 60 * 4
+        end = now
+
+    c = conn.cursor()
+
+    rows = c.execute('''
+       select time, up_speed, down_speed from bandwidth_results
+           where time >= ? and time <= ?
+           order by time;
+    ''', (start, end)).fetchall()
+
+    plt.plot(
+        [row[0] for row in rows],
+        [row[1]/1000**2 for row in rows],
+    )
+    plt.plot(
+        [row[0] for row in rows],
+        [row[2]/1000**2 for row in rows],
+    )
+    plt.title('speedtest')
+    plt.ylabel('Mbps')
+    xmin = start
+    xmax = end
+    plt.xlim(xmin, xmax)
+    r = range(7)
+    xticks = [interpolate(xmin, xmax, x / float(len(r)-1)) for x in r]
+    plt.xticks(
+       xticks,
+       [epoch_to_human(x) for x in xticks]
+    )
+    plt.tight_layout()
+    plt.grid(True, linestyle='--')
+    svg=StringIO.StringIO()
+    plt.savefig(svg, format='svg')
+    plt.close()
+    svg.seek(0)
+
+    return render_template('bw.txt', svg=svg.buf)
